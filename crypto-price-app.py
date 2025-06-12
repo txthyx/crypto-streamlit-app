2c3fde284c534f21b204543c59d876eeddad4e90
txthyx <txthyx@icloud.com>
2025-06-12 13:36:27 +0530

    Update crypto-price-app.py

--- crypto-price-app.py
+++ crypto-price-app.py
@@ -11,7 +11,7 @@ import base64
 st.set_page_config(layout="wide", page_title="Crypto Price Tracker")
 
 with st.sidebar:
-    st.image("logo.png", width=200)
+    st.image("logo.png", width=220)
 
 #---------------------------------#
 # Title
